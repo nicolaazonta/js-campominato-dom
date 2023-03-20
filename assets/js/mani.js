e91019bc918c84c3ee3cd1d8cbb79ab941b84eac
nicolaazonta <nicolazonta114@gmail.com>
2023-03-20 12:05:08 +0100

    structure random not ripetitive creating system

--- assets/js/mani.js
+++ assets/js/mani.js
@@ -12,31 +12,12 @@ con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 
 //creare una griglia di 100 caselle
 
 
-const createButtonEl = document.getElementById('create_button')
+const createButtonEl = document.getElementById('create_button');
 
-
-
-/* const difficultAnchor = document.getElementById('difficult_choice');
-
-const difficultChoice = difficultAnchor.value;
-
-console.log(difficultChoice); */
-
-
-
-
-/* var e = document.getElementById("ddlViewBy");
-var value = e.value;
-var text = e.options[e.selectedIndex].text; */
-
-//if()
-
-
-createButtonEl.addEventListener('click' , function(){   
-
-    
+createButtonEl.addEventListener('click' , function(){      
     const difficultChoice = Number(document.getElementById('difficult_choice').value);
 
+    // the user choose the difficult of the game
     let difficultGradient; 
     let numberOfSquares;
 
@@ -51,13 +32,28 @@ createButtonEl.addEventListener('click' , function(){
         numberOfSquares = 49;
     } 
 
+    bombs(numberOfSquares);
+
+
+    // create the squares grid (100-easy / 91-medium / 49-hard)
     for (let i = 0; i < numberOfSquares; i++) {
+
+        //Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+
+
+
+
+
+
         const containerEl = document.querySelector('.container');
         const creatingSquare = `<div class="square ${difficultGradient} d-flex justify-content-center align-items-center"><h5>${i + 1}</h5>
         </div>`
         containerEl.insertAdjacentHTML('beforeend' , creatingSquare);    
     }
 
+
+    //the user can interact with the grid changing the squares color
     const allSquares = document.querySelectorAll('.square');
 
     for (let i = 0; i < allSquares.length ; i++) {
@@ -72,6 +68,41 @@ createButtonEl.addEventListener('click' , function(){
     }
 
 })
+
+
+function bombs(numberOfSquares) {
+
+    let bombsArray = [];
+
+    let k = 0;
+    while (k < 16) {
+
+        let randomNumberAnchor = Math.floor(Math.random() * numberOfSquares) + 1;
+
+
+        //if che incrementa k se il numero non si ripete e che aggiunge lo stesso numero all'array 
+        if (randomNumberAnchor === (bombsArray[k - 1]) ){}
+        else {
+
+            bombsArray.push = randomNumberAnchor;
+
+            console.log(randomNumberAnchor);
+
+            k++
+        }        
+    }     
+    return bombsArray;
+}
+
+
+//In seguito l'utente clicca su una cella:
+//se il numero è presente nella lista dei numeri generati abbiamo calpestato una bomba la cella si colora di rosso e la partita termina.
+
+//Altrimenti la cella cliccata si colora di azzurro. l'utente può continuare a cliccare sulle altre celle.
+
+//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. 
+
 
 
 
